928a64e95a4943526fb408917d4e1f8a3183ef24
sashi-cse <sashikant112005@gmail.com>
2026-07-07 07:35:50 +0000

    prototypes

---
+++ 10_classes_and_oop/prototypes.js
@@ -0,0 +1,67 @@
+// let myName = "sashi     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.sashi = function(){
+    console.log(`sashi is present in all objects`);
+}
+
+Array.prototype.heysashi = function(){
+    console.log(`sashi says hello`);
+}
+
+// heroPower.sashi()
+// myHeros.sashi()
+// myHeros.heysashi()
+// heroPower.heysashi()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"sashi".trueLength()
+"iceTea".trueLength()
